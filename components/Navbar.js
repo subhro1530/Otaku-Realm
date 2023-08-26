b74eae71103651a8fa7b17191db8b8a61425e8ff
subhro1530 <shaswata.ssaha@gmail.com>
2023-08-26 12:08:01 +0530

    made the history sidebar better

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,40 +1,77 @@
-import React, { useState } from "react";
-import Link from "next/link";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 
 const Navbar = () => {
   const router = useRouter();
   const [menuOpen, setMenuOpen] = useState(false);
+  const [historyOpen, setHistoryOpen] = useState(false);
+  const [searchHistory, setSearchHistory] = useState([]);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
+    document.body.classList.toggle("menu-open"); // Add class to body for blurring effect
+  };
+
+  const toggleHistory = () => {
+    setHistoryOpen(!historyOpen);
+  };
+
+  const handleSearch = (term) => {
+    // Store search term in local storage
+    const updatedHistory = [...searchHistory, term];
+    localStorage.setItem("searchHistory", JSON.stringify(updatedHistory));
+    setSearchHistory(updatedHistory);
   };
 
   return (
-    <nav className="navbar">
+    <nav className={`navbar ${menuOpen ? "open" : ""}`}>
       <div className="logo">
         <img src="logo.png" alt="Logo" />
       </div>
       <div className="menu-toggle" onClick={toggleMenu}>
-        <div className={`hamburger ${menuOpen ? "open" : ""}`}></div>
+        <i className={`fa-solid fa-bars ${menuOpen ? "open" : ""}`}></i>
       </div>
       <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
         <li>
-            <a
-              href="/"
-              passHref
-              className={router.pathname === "/" ? "active" : ""}
-            >
-              Home
-            </a>
+          <a
+            href="/"
+            passHref
+            className={`nav-link ${router.pathname === "/" ? "active" : ""}`}
+          >
+            Home
+          </a>
         </li>
         <li>
-          <a href="/about" passHref>
+          <a
+            href="/about"
+            passHref
+            className={`nav-link ${
+              router.pathname === "/about" ? "active" : ""
+            }`}
+          >
             About
           </a>
         </li>
+        <li>
+          <button className="history-button" onClick={toggleHistory}>
+            History
+          </button>
+        </li>
       </ul>
-      <button className="history-button">History</button>
+      {/* History Sidebar */}
+      <div className={`history-sidebar ${historyOpen ? "open" : ""}`}>
+        <div className="history-sidebar-content">
+          <h2>Search History</h2>
+          <ul>
+            {searchHistory.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="history-sidebar-close" onClick={toggleHistory}>
+          &#10005;
+        </div>
+      </div>
     </nav>
   );
 };
--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,18 @@ function MyApp({ Component, pageProps }) {
   return (
     <div>
       {" "}
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap"
+        rel="stylesheet"
+      ></link>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+        integrity="sha384-59p0Bj1t/0TdKJKM6VOrDufqKsN51t9WEa5xy5jQYn79JX4yZg4Ig4K+4RfNkC7B"
+        crossorigin="anonymous"
+      />
       {/* Wrap everything in a parent element */}
       <Navbar />
       <PageProvider>
